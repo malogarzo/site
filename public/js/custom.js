decc1a14621e8fd04706c52943596552e99fe52b
Mariano Logarzo <malogarzo@Marianos-MacBook-Pro.local>
2016-04-12 21:10:58 -0300

    social links update

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,7 +9,7 @@
         var link = $(this).attr('href');
         var link1 = '.'+link;
         if (!$(link1).hasClass('active')) {
-            $('.content.active').animate({top: '20px', opacity: '0'}, 700, function(){
+            $('.content.active').animate({top: '0', opacity: '0'}, 100, function(){
                 $('.content.active').css('top','0');
                 $('.content.active').css('opacity','1');
                 $('.content').removeClass('active');
